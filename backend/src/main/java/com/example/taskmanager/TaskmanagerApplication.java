95a4efd868fb0cd8950c4e0ac8ee92b6e8623103
Vladik Kangur <vladik.dudkin111@gmail.com>
2026-06-07 20:57:25 +0300

    Fix backend startup on laptop

--- backend/src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ backend/src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskmanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskmanagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerApplication.class, args);
+    }
 }
